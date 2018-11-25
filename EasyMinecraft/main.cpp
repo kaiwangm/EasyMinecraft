29d316aa51cae808b3b2b72a317f311e81148ece
VNKMX <VNKMXX@Gmail.com>
2018-11-26 01:01:15 +0800

    初始化opengl

---
+++ EasyMinecraft/main.cpp
@@ -0,0 +1,38 @@
+#define GLEW_STATIC
+#include<iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Minecraft", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "open windows filed." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "init glew filed." << std::endl;
+	}
+
+	glViewport(0, 0, 800, 600);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
